4fcc5a93d79a86de27027a46f7b1fe7a7551e50b
agusdon <agusdon@gmail.com>
2017-04-25 00:07:58 -0400

    Update fars_functions.R

--- R/fars_functions.R
+++ R/fars_functions.R
@@ -1,24 +1,29 @@
 #' Loads a CSV file
-#' 
-#' This function reads in raw data from a csv file
-#' 
+#'
+#' @description
+#' The function loads a CSV file defined by \code{filename} argument and returns
+#' a tibble. If the path is incorrect the function will end with an error.
+#'
 #' @param filename Path to the CSV file (character)
-#' @details Will return an error message if the file does not exist
-#' @return The function returns a tibble (data.frame) from the CSV file loaded
-#' @importFrom readr read_csv
-#' @importFrom dplyr tbl_df 
-#' @examples \dontrun{
-#' accident_2014 <- fars_read("./data/accident_2014.csv.bz2")
+#'
+#' @return The function returns a tibble (data.frame) based on the CSV file.
+#'
+#' @examples
+#' \dontrun{
 #' accident_2015 <- fars_read("./data/accident_2015.csv.bz2")
 #' }
+#'
+#' @importFrom readr read_csv
+#' @importFrom dplyr tbl_df
+#'
 #' @export
 fars_read <- function(filename) {
-  if(!file.exists(filename))
-    stop("file '", filename, "' does not exist")
-  data <- suppressMessages({
-    readr::read_csv(filename, progress = FALSE)
-  })
-  dplyr::tbl_df(data)
+        if(!file.exists(filename))
+                stop("file '", filename, "' does not exist")
+        data <- suppressMessages({
+                readr::read_csv(filename, progress = FALSE)
+        })
+        dplyr::tbl_df(data)
 }
 
 #' Creates a new filename
